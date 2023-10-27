b302822ed948615f8337b3cceaa47580cabe96cd
s-a-y-o-n <sayonsibi2708@gmail.com>
2023-10-27 16:27:33 +0530

    commit

--- lib/splash.dart
+++ lib/splash.dart
@@ -11,11 +11,20 @@ class Splash extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Colors.red,
       body: Center(
-        child: Icon(
-          Icons.movie_sharp,
-          size: 200,
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // Icon(
+            //   Icons.movie_sharp,
+            //   size: 200,
+            // ),
+            
+            Image.asset("assets/icons/death_evil_fantasy_grim reaper_icon.png",height: 200,width: 200),
+           
+            // Image.network("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSitRR1YjtF86dsqjrS6mlhXp14y7R9Nk9LeQ&usqp=CAU"),
+            Text("Woooaaaaaa", style: TextStyle(fontSize: 30, color: Colors.white,fontWeight: FontWeight.bold)),
+          ],
         ),
       ),
     );
