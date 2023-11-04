da3deb85008ae992bc37a9efa2d5a85a3b9fc2d7
s-a-y-o-n <sayonsibi2708@gmail.com>
2023-11-04 12:16:00 +0530

    commit

--- lib/signuppage.dart
+++ lib/signuppage.dart
@@ -10,7 +10,6 @@ class SignupPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login Page'),
         backgroundColor: Colors.white60,
         elevation: 0,
         leading: const Icon(Icons.arrow_back, color: Colors.black,),
@@ -20,76 +19,82 @@ class SignupPage extends StatelessWidget {
         //   Icon(Icons.search)
         // ],
       ),
-      body: Column(
-        children: [
-          Text('SIGNUP', style: GoogleFonts.atomicAge(fontSize: 30, fontWeight: FontWeight.bold),),
-          Padding(
-            padding: EdgeInsets.only(left: 22,right: 22,top: 12),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.person),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
-                hintText: "Name",
-                labelText: "Name",
+      body: Padding(
+        padding: const EdgeInsets.only(left: 50,right: 50),
+        child: Column(mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('SIGNUP', style: GoogleFonts.playfairDisplay(fontSize: 30, fontWeight: FontWeight.bold),),
+            SizedBox(height: 10,),
+            Text('Create an account, its free.',style: GoogleFonts.openSans(fontSize: 15),),
+            SizedBox(height: 20,),
+            Padding(
+              padding: EdgeInsets.only(left: 22,right: 22,top: 12),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.person),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
+                  hintText: "Name",
+                  labelText: "Name",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 22,right: 22,top: 12),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
-                hintText: "Email",
-                labelText: "Email",
+            Padding(
+              padding: EdgeInsets.only(left: 22,right: 22,top: 12),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.email),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30),),
+                  hintText: "Email",
+                  labelText: "Email",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 22,right: 22,top: 12,bottom: 10),
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.key_off),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            Padding(
+              padding: EdgeInsets.only(left: 22,right: 22,top: 12,bottom: 10),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.key_off),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
 
-                hintText: "Password",
-                labelText: "Password",
+                  hintText: "Password",
+                  labelText: "Password",
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(left: 22,right: 22,top: 12,bottom: 10),
-            child: TextField(
-              decoration: InputDecoration(
-                prefixIcon: Icon(Icons.key),
-                border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
+            Padding(
+              padding: EdgeInsets.only(left: 22,right: 22,top: 12,bottom: 10),
+              child: TextField(
+                decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.key),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.circular(30)),
 
-                hintText: "Confirm Password",
-                labelText: "Confirm Password",
+                  hintText: "Confirm Password",
+                  labelText: "Confirm Password",
+                ),
               ),
             ),
-          ),
-          MaterialButton(
-            color: Colors.green,
-            shape: StadiumBorder(),
-            height: 60,
-            minWidth: 100,
-            onPressed: (){}, child: Text('Signup'),),
-          SizedBox(height: 20,),
-          TextButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
-            },
-            child: RichText(text:  TextSpan(
-                children: [
-                  TextSpan(text: "Already have an account?"),
-                  TextSpan(text: "Signin.", style: TextStyle(fontWeight: FontWeight.bold))
-                ]
-            )),
-          )
+            MaterialButton(
+              color: Colors.redAccent,
+              shape: StadiumBorder(),
+              height: 60,
+              minWidth: 300,
+              onPressed: (){}, child: Text('Signup'),),
+            SizedBox(height: 20,),
+            TextButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginPage()));
+              },
+              child: RichText(text:  TextSpan(
+                  children: [
+                    TextSpan(text: "Already have an account?"),
+                    TextSpan(text: "Sign in.", style: TextStyle(fontWeight: FontWeight.bold))
+                  ]
+              )),
+            )
 
-        ],
+          ],
+        ),
       ),
 
     );
