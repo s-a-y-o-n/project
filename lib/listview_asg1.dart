3461ceb050bee2f00dffdea99e72fe24ac446f58
s-a-y-o-n <sayonsibi2708@gmail.com>
2023-11-10 17:04:33 +0530

    commit

--- lib/listview_asg1.dart
+++ lib/listview_asg1.dart
@@ -8,9 +8,7 @@ void main(){
 }
 class Listview_asg1 extends StatelessWidget {
 
-
   var months=["January", "February","March", "April","May","June","July","August","September","October","November","December"];
-
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +23,15 @@ class Listview_asg1 extends StatelessWidget {
         );
       }, separatorBuilder: (context,index){
         if(index%4==0){
-          return Card(color: Colors.redAccent,
+          return const Card(color: Colors.redAccent,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0),
             child: Text('Advertisement', style: TextStyle(color: Colors.white),),
           ),);
         }
         else
           {
-            return Divider();
+            return const Divider();
           }
       }, itemCount: 12),
     );
