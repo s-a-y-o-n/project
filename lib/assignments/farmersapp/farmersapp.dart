f65636c9f0da34e04d9371a1df68da048d2b6093
s-a-y-o-n <sayonsibi2708@gmail.com>
2023-12-27 01:09:23 +0530

    commit

--- lib/assignments/farmersapp/farmersapp.dart
+++ lib/assignments/farmersapp/farmersapp.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:project/assignments/farmersapp/data.dart';
 
 class Farmershome extends StatelessWidget {
@@ -19,37 +20,12 @@ class Farmershome extends StatelessWidget {
             elevation: 5,
             snap: true,
             backgroundColor: Colors.green,
-            expandedHeight: 130,
-            flexibleSpace: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 40, left: 10),
-                  child: Text(
-                    'FARMERS FRESH ZONE',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white),
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Container(
-                    width: w - 20,
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: const TextField(
-                      decoration: InputDecoration(
-                          hintText: 'Search', prefixIcon: Icon(Icons.search)),
-                    ),
-                  ),
-                ),
-              ],
+            title: Text(
+              'FARMERS FRESH ZONE',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
             actions: [
               const Icon(
@@ -70,6 +46,17 @@ class Farmershome extends StatelessWidget {
                 ),
               ),
             ],
+            bottom: AppBar(
+              backgroundColor: Colors.green,
+              title: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: 'Search',
+                  prefixIcon: Icon(Icons.search),
+                ),
+              ),
+            ),
           ),
           SliverList(
               delegate: SliverChildListDelegate([
@@ -358,7 +345,256 @@ class Farmershome extends StatelessWidget {
                       ),
                   childCount: products.length),
               gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2, mainAxisExtent: 322))
+                  crossAxisCount: 2, mainAxisExtent: 322)),
+          SliverList(
+              delegate: SliverChildListDelegate([
+            ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                'VIEW MORE',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStatePropertyAll(Colors.green),
+                  minimumSize: MaterialStatePropertyAll(Size(w, 40))),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Container(
+              width: w,
+              height: 220,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: NetworkImage(
+                          "https://media.istockphoto.com/id/499695105/photo/acidic-food.jpg?s=612x612&w=0&k=20&c=avxH4Xu92_z_fd5b8wUWCi3RdDKhXzKcstsu27VsSiI="),
+                      fit: BoxFit.fill)),
+            ),
+            SizedBox(
+              height: 25,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: List.generate(
+                    5,
+                    (index) => Card(
+                          child: Container(
+                            height: 265,
+                            width: 200,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Container(
+                                  width: 200,
+                                  height: 150,
+                                  decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                          image: NetworkImage(
+                                              blogs[index]["image"]),
+                                          fit: BoxFit.fill)),
+                                ),
+                                Text(
+                                  blogs[index]["title"],
+                                  style: TextStyle(fontSize: 15),
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text('a year ago',
+                                          style: TextStyle(
+                                              fontSize: 10,
+                                              color: Color.fromARGB(
+                                                  255, 53, 53, 53))),
+                                      Icon(
+                                        Icons.arrow_forward,
+                                        color: Colors.green,
+                                      )
+                                    ],
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        )),
+              ),
+            ),
+            MaterialButton(
+              onPressed: () {},
+              child: Text(
+                'VIEW MORE',
+                style: TextStyle(
+                    color: Colors.green,
+                    fontSize: 15,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                'What Our Customers Say?',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+            CarouselSlider.builder(
+                itemCount: 4,
+                itemBuilder: (context, index, int pageViewIndex) {
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(review[index]["image"]),
+                      radius: 25,
+                    ),
+                    title: Text(
+                      review[index]["name"],
+                      style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text(review[index]["content"]),
+                  );
+                },
+                options: CarouselOptions(
+                    height: 300, viewportFraction: 1, autoPlay: true)),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 80),
+              child: Text(
+                'This Kochi based farm-to-fork online marketplace is connecting farmers directly to customers',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.asset(
+                  "assets/icons/The_Economic_Times_logo.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/icons/thehindu.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/icons/yourstory.png",
+                  height: 80,
+                  width: 80,
+                ),
+                Image.asset(
+                  "assets/icons/indianexpress.png",
+                  height: 80,
+                  width: 80,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Get To Know Us',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Wrap(
+              alignment: WrapAlignment.start,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'About Us',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Our Farmers',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Blog',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Text(
+              'Useful Links',
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+            ),
+            Wrap(
+              alignment: WrapAlignment.start,
+              children: [
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Privacy Policy',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Return & Refund Policy',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+                TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      'Terms & Conditions',
+                      style: TextStyle(color: Color.fromARGB(255, 59, 59, 59)),
+                    )),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(FontAwesomeIcons.twitter)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(FontAwesomeIcons.youtube)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(FontAwesomeIcons.facebook)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(FontAwesomeIcons.instagram)),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.green,
+              height: 100,
+              child: Center(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 70),
+                  child: Text(
+                    'Copyright @ 2021 Farmers Fresh Zone. All Rights Reserved. V 2.40.51',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              ),
+            )
+          ]))
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
